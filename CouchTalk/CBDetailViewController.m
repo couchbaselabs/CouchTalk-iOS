a2f82d976d1a3c12d22db55258c2fe99e7f1cc4b
Nathan Vander Wilt <nate@calftrail.com>
2014-05-09 15:56:33 -0700

    get rid of default text in iPad

--- CouchTalk/CBDetailViewController.m
+++ CouchTalk/CBDetailViewController.m
@@ -183,7 +183,7 @@
 
 - (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
 {
-    barButtonItem.title = NSLocalizedString(@"Master", @"Master");
+    barButtonItem.title = NSLocalizedString(@"Rooms", @"Rooms");
     [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
     self.masterPopoverController = popoverController;
 }
